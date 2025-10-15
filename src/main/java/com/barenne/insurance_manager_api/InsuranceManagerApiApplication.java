042b09a68cf646a4949061cdd2bb6767bfc02048
MarieBrnn <barenne.marie@icloud.com>
2025-10-15 20:15:01 +0200

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/barenne/insurance_manager_api/InsuranceManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.barenne.insurance_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barenne/insurance_manager_api/InsuranceManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barenne.insurance_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
